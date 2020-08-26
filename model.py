552007deaae9b2d0009cd73350a743def231429d
Daniel Iong <daniong@daniong-mbp.local>
2020-08-26 15:17:35 -0400

    added ARX model class, added NAs in front of predictions

--- model.py
+++ model.py
@@ -7,9 +7,10 @@ from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
 from sklearn.linear_model import LinearRegression
 from sklearn.utils.estimator_checks import check_estimator
 from sklearn.utils.validation import check_is_fitted, check_X_y, check_array, check_consistent_length
-from sklearn.metrics.regression import mean_squared_error
+from sklearn.metrics import mean_squared_error
 
 from GeoMagTS.data_preprocessing import LagFeatureProcessor, TargetProcessor
+from GeoMagTS.utils import _get_NA_mask
 
 class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
     def __init__(self,
@@ -33,7 +34,7 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
         self.transformer_X = transformer_X
         self.transformer_y = transformer_y
 
-    def fit(self, X, y, storm_labels=None, **fit_params):
+    def fit(self, X, y, storm_labels=None, **fit_args):
 
         self.lag_feature_processor_ = LagFeatureProcessor(
             auto_order=self.auto_order,
@@ -45,13 +46,13 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
             storm_labels=storm_labels)
 
         X, y = check_X_y(X, y, y_numeric=True)
+
+        concat_data = np.concatenate([y.reshape(-1, 1), X], axis=1)
         if self.transformer_X is not None:
-            X = self.transformer_X.fit_transform(X)
+            concat_data = self.transformer_X.fit_transform(concat_data)
         if self.transformer_y is not None:
             y = self.transformer_y.fit_transform(y.reshape(-1, 1)).flatten()
-
-        y_X_ = np.concatenate([y.reshape(-1, 1), X], axis=1)
-        X_ = self.lag_feature_processor_.fit_transform(y_X_)
+        X_ = self.lag_feature_processor_.fit_transform(concat_data)
         y_ = self.target_processor_.fit_transform(y)
 
         # # IDEA: Put this in processors
@@ -60,7 +61,8 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
         # y_ = check_array(y_, force_all_finite='allow-nan', ensure_2d=False)
 
         # Remove NAs induced by LagFeatureProcessor, TargetProcessor
-        target, features = self._removeNA(X_, y_)
+        X_y_mask = _get_NA_mask(X_, y_)
+        target, features = X_[~X_y_mask], y_[~X_y_mask]
 
         if self.base_estimator is None:
             # Default estimator is LinearRegression
@@ -68,7 +70,7 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
         else:
             self.base_estimator_fitted_ = clone(self.base_estimator)
 
-        self.base_estimator_fitted_.fit(target, features, **fit_params)
+        self.base_estimator_fitted_.fit(target, features, **fit_args)
         return self
 
     def predict(self, X, y, storm_labels=None):
@@ -79,46 +81,39 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
             exog_order=self.exog_order,
             target_column=0,
             storm_labels=storm_labels)
-        target_processor = TargetProcessor(
-            pred_step=self.pred_step,
-            storm_labels=storm_labels)
 
         X, y = check_X_y(X, y, y_numeric=True)
+        concat_data = np.concatenate([y.reshape(-1, 1), X], axis=1)
         if self.transformer_X is not None:
-            X = self.transformer_X.transform(X)
+            concat_data = self.transformer_X.transform(concat_data)
         if self.transformer_y is not None:
             y = self.transformer_y.transform(y.reshape(-1, 1)).flatten()
 
-        concat_data = np.concatenate([y.reshape(-1, 1), X], axis=1)
-        y_X_ = lag_feature_processor.transform(concat_data)
-        features = self._removeNA(y_X_)
+        X_ = lag_feature_processor.fit_transform(concat_data)
+        X_mask = _get_NA_mask(X_)
+        features = X_[~X_mask]
 
-        ypred = self.base_estimator_fitted_.predict(features)
+        y_pred_ahead = self.base_estimator_fitted_.predict(features)
 
         if self.transformer_y is not None:
-            ypred = self.transformer_y.inverse_transform(ypred)
+            y_pred_ahead = self.transformer_y.inverse_transform(y_pred_ahead)
 
-        return ypred
+        y_pred = np.empty(y.shape[0])
+        y_pred[X_mask], y_pred[~X_mask] = np.nan, y_pred_ahead  
 
-    def _removeNA(self, X, y=None):
-        if y is None:
-            mask = safe_mask(X, np.isnan(X).any(axis=1))
-            return X[~mask]
-        else:
-            data_ = np.concatenate([y.reshape(-1, 1), X], axis=1)
-            mask = np.isnan(data_).any(axis=1)
-            # Check mask
-            mask = safe_mask(X, mask)
-            mask = safe_mask(y, mask)
-            X_, y_ = check_X_y(X[~mask], y[~mask], y_numeric=True)
-            return X_, y_
+        y_pred_aligned = np.concatenate(
+            [np.empty(self.pred_step)*np.nan, y_pred]
+            )[0:len(y)]
+ 
+        return y_pred_aligned
+
 
     def score(self, X, y, storm_labels=None, squared=True):
         # TODO: Error handling and more options
         y_pred = self.predict(X, y, storm_labels)
-        mask = np.isnan(y) | np.isnan(y_pred)
+        # mask = np.isnan(y) | np.isnan(y_pred)
 
-        return mean_squared_error(y[~mask], y_pred[~mask], squared=squared)
+        return mean_squared_error(y, y_pred, squared=squared)
 
     def plot_predict(self, X, y, storm_labels=None, times=None, display_info=False, **plot_params):
         y_pred = self.predict(X, y, storm_labels)
@@ -126,12 +121,14 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
 
         fig, ax = plt.subplots(sharex=True, figsize=(10, 7), **plot_params)
         if times is None:
-            ax.plot(y[-len(y_pred):], label='Truth',
+            ax.plot(y, label='Truth',
                     color='black', linewidth=0.5)
             ax.plot(y_pred,
                     label=str(self.pred_step)+'-step ahead prediction', color='red', linewidth=0.5)
         else:
-            ax.plot(times, y[-len(y_pred):],
+            # TODO: Check if this subsetting is valid. Find better to do it?
+            times = times
+            ax.plot(times, y,
                     label='Truth', color='black', linewidth=0.5)
             ax.plot(times, y_pred,
                     label=str(self.pred_step)+'-step ahead prediction', color='red', linewidth=0.5)
@@ -153,3 +150,36 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
                 'no_validation': True}
 
 # estimator_checks = check_estimator(GeoMagTSRegressor())
+
+class GeoMagARX(GeoMagTSRegressor):
+    def __init__(self, auto_order=10, 
+                 exog_order=10, 
+                 pred_step=1, 
+                 transformer_X=None, 
+                 transformer_y=None, 
+                 **lm_params):
+        base_estimator = LinearRegression()
+        super().__init__(base_estimator=base_estimator, auto_order=auto_order, exog_order=exog_order, pred_step=pred_step, transformer_X=transformer_X, transformer_y=transformer_y, **lm_params)
+    
+    def get_coef_df(self, feature_columns):
+        check_is_fitted(self)
+        ar_coef_names = np.array(
+            ["ar"+str(i) for i in range(self.auto_order)]
+            )
+        exog_coef_names = np.concatenate(
+            [[x+str(i) for i in range(self.exog_order)] 
+             for x in feature_columns]
+            ).T
+        coef_names = np.concatenate([ar_coef_names, exog_coef_names])
+        
+        coef = pd.Series(
+            self.base_estimator_fitted_.coef_, index=coef_names)
+        coef_df = pd.DataFrame(
+            {col: coef[coef.index.str.contains(col+'[0-9]+$')].reset_index(drop=True)
+             for col in ['ar']+feature_columns
+             }
+            )
+        coef_df['ar'] = coef_df['ar'].shift(1)
+              
+        return coef_df
+    
