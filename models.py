8fea36a098caa23faf9c9f2c6a2d713f62b8db1c
Daniel Iong <daniong@daniong-mbp.local>
2020-09-10 21:28:42 -0400

    changes to plot function and get_coef_df in GeoMagARX

--- models.py
+++ models.py
@@ -7,17 +7,19 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 from sklearn.utils import safe_mask
 from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.utils.estimator_checks import check_estimator
 from sklearn.utils.validation import check_is_fitted, check_X_y, check_array, check_consistent_length
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, make_scorer
 from sklearn.pipeline import Pipeline
 from pandas.tseries.frequencies import to_offset
+from sklearn.preprocessing import PolynomialFeatures
 
 from GeoMagTS.data_preprocessing import PropagationTimeProcessor, FeatureProcessor, TargetProcessor
 from GeoMagTS.utils import _get_NA_mask
 
 # NOTE: Converting to pandas from np slowed down the code significantly
+
 
 class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
     def __init__(self,
@@ -40,11 +42,12 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
         self.propagate = propagate
         self.time_resolution = time_resolution
         self.D = D
+        self.estimator_params = estimator_params
 
     def _check_X(self, X, check_multi_index=True, check_vx_col=True, check_same_cols=False):
         if not isinstance(X, pd.DataFrame):
             raise TypeError("X must be a pandas object (for now).")
-        
+
         if isinstance(X.index, pd.MultiIndex):
             if X.index.nlevels < 2:
                 raise ValueError(
@@ -52,18 +55,17 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
         elif check_multi_index:
             raise TypeError("X must have a MultiIndex (for now).")
         elif not isinstance(X.index, pd.DatetimeIndex):
-                raise TypeError("Time index must be of type pd.DatetimeIndex.")
-        
+            raise TypeError("Time index must be of type pd.DatetimeIndex.")
 
         # Check if vx_colname is in X
         if check_vx_col and self.vx_colname_ not in X.columns:
             raise ValueError("X does not have column "+self.vx_colname_+".")
-        
+
         if check_same_cols:
             if not X.columns.equals(self.features_):
                 raise ValueError(
                     "X used for predicting must have the same columns as the X used for training.")
-        
+
         dupl_mask_ = X.index.duplicated(keep='last')
         if dupl_mask_.any():
             warnings.warn(
@@ -101,6 +103,8 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
         # Input validation
         X, y = self._check_inputs(X, y)
 
+        # self.base_estimator = self.base_estimator.set_params(
+        #     **self.estimator_params)
         self.features_ = X.columns
 
         time_res_minutes = to_offset(self.time_resolution).delta.seconds/60
@@ -118,6 +122,7 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
         y_ = self.target_processor_.fit_transform(
             y, storm_level=self.storm_level_, time_level=self.time_level_)
 
+
         if self.propagate:
             self.propagation_time_processor_ = PropagationTimeProcessor(
                 Vx=X[self.vx_colname_],
@@ -125,9 +130,6 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
                 D=self.D)
             y_ = self.propagation_time_processor_.fit_transform(
                 y_, time_level=self.time_level_)
-
-        target_mask = self._get_target_mask()
-        X, y = X[target_mask], y[target_mask]
 
         # Process features
         self.feature_processor = FeatureProcessor(
@@ -141,6 +143,11 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
             storm_level=self.storm_level_,
             time_level=self.time_level_)
 
+        target_mask = self._get_target_mask()
+        X_ = X_[target_mask]
+
+        # BUG: lengths of X_, y_ differ when pred_step > 0
+        
         mask = _get_NA_mask(X_, y_)
         target, features = X_[mask], y_[mask]
 
@@ -164,9 +171,9 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
             return mask
 
     def predict(self, X, y):
-        
+
         check_is_fitted(self)
-        X, _ = self._check_X(X, check_multi_index=False, 
+        X, _ = self._check_X(X, check_multi_index=False,
                              check_vx_col=False, check_same_cols=True)
 
         test_feature_processor = FeatureProcessor(
@@ -223,7 +230,7 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
             test_prop_time_processor.fit(y_empty)
             pred_times = test_prop_time_processor.propagated_times_
             ypred_processed = pd.Series(ypred, index=pred_times)
-            
+
         return ypred_processed
 
     # TODO: Add interactive plot
@@ -233,6 +240,9 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
                      figsize=(15, 10),
                      save=True,
                      file_name='prediction_plot.pdf',
+                     plot_persistence=True,
+                     model_name=None,
+                     sw_to_plot=None,
                      **plot_params):
         check_is_fitted(self)
 
@@ -251,64 +261,147 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
 
         ypred = self.predict(X, y)
 
+        if save:
+            pdf = PdfPages(file_name)
+
+        for storm in storms_to_plot:
+
+            # rmse = self.score(X.loc[storm], y.loc[storm])
+            fig, ax = self._plot_one_storm(
+                X.loc[storm], y.loc[storm],
+                storm_idx=storm, display_info=display_info, 
+                figsize=figsize, plot_persistence=plot_persistence,
+                model_name=model_name, sw_to_plot=sw_to_plot, **plot_params)
+
+            if save:
+                pdf.savefig(fig)
+            else:
+                plt.show()
+
+        if save:
+            pdf.close()
+        return None
+
+    def _plot_one_storm(
+            self, X, y, storm_idx, display_info=False,
+            figsize=(10, 7), plot_persistence=True, model_name=None, 
+            sw_to_plot=None, **plot_params):
+
+        if sw_to_plot is not None:
+            n_sw_to_plot = len(sw_to_plot)
+            fig, ax = plt.subplots(nrows=n_sw_to_plot+1, 
+                                   ncols=1,
+                                   sharex=True,
+                                   figsize=figsize,
+                                   gridspec_kw={
+                                       'height_ratios':[4]+[1]*n_sw_to_plot},
+                                   **plot_params)
+            ax0 = ax[0]
+        else:
+            fig, ax = plt.subplots(sharex=True,
+                                   figsize=figsize,
+                                   **plot_params)
+            ax0 = ax
+        
+        ### Plot truth
+        ax0.plot(y, label='Truth', color='black', linewidth=0.5)
+        
+        ### Plot predictions
+        ypred = self.predict(X, y)
+        rmse = self.score(X, y, negative=False, round=True, decimals=2)
+    
         # Get prediction label
         pred_label = ''
         if self.propagate:
             pred_label = pred_label + 'Propagated '
         if self.pred_step > 0:
             pred_label = pred_label + str(self.pred_step) + 'min. ahead'
-        pred_label = pred_label + 'prediction'
+        pred_label = pred_label + 'prediction (RMSE: '+ str(rmse)+')'
+        
+        ax0.plot(ypred, label=pred_label, color='red', linewidth=0.5)
 
-        if save:
-            pdf = PdfPages(file_name)
-
-        for storm in storms_to_plot:
+        if plot_persistence:
+            y_persistence = self._predict_persistence(X, y)
+            rmse_persistence = self.score(
+                X, y, persistence=True, squared=False, round=True, decimals=2)
             
-            rmse = self.score(X.loc[storm], y.loc[storm])
-            fig, ax = self._plot_one_storm(
-                y.loc[storm], ypred.loc[storm], 
-                rmse=rmse, storm_idx=storm, pred_label=pred_label, display_info=display_info, figsize=figsize)
-            if save:
-                pdf.savefig(fig)
-            else:
-                plt.show()
-                
-        if save:
-            pdf.close()
-        return None
+            persistence_label = 'Persistence (RMSE: '+str(rmse_persistence)+')'
+            ax0.plot(y_persistence, label=persistence_label,
+                    color='blue', linestyle='--', linewidth=0.5)
 
-    def _plot_one_storm(
-            self, y, ypred, rmse, storm_idx, pred_label, display_info=False,
-            figsize=(10, 7), **plot_params):
-
-        fig, ax = plt.subplots(sharex=True,
-                               figsize=figsize,
-                               **plot_params)
-        ax.plot(y, label='Truth', color='black', linewidth=0.5)
-        ax.plot(ypred, label=pred_label, color='red', linewidth=0.5)
-        ax.legend()
+        ax0.legend()
+        # Adjust time scale
         locator = mdates.AutoDateLocator(minticks=15)
         formatter = mdates.ConciseDateFormatter(locator)
-        ax.xaxis.set_major_locator(locator)
-        ax.xaxis.set_major_formatter(formatter)
+        ax0.xaxis.set_major_locator(locator)
+        ax0.xaxis.set_major_formatter(formatter)
 
         if display_info:
-            ax.set_title(
-                'Storm #'+str(storm_idx)+": " +
-                'auto_order='+str(self.auto_order)+', ' +
-                'exog_order='+str(self.exog_order)+' (in minutes), ' +
-                'RMSE='+str(np.round(rmse, decimals=2)))
-
+            info = 'Storm #'+str(storm_idx)+": " + \
+            'auto_order='+str(self.auto_order)+', ' + \
+            'exog_order='+str(self.exog_order)+' (in min.)'
+            if model_name is not None:
+                info = info + ', ' + model_name 
+            
+            if len(self.estimator_params) != 0:
+                info = info + ' ('
+                i = 0
+                for param, value in self.estimator_params.items():
+                    info = info + param + '=' + str(value)
+                    if i != len(self.estimator_params)-1:
+                        info = info + ', '
+                        i = i + 1
+                info = info + ')'
+            ax0.set_title(info)
+        
+        if sw_to_plot is not None:
+            
+            for i in range(n_sw_to_plot):
+                ax[i+1].plot(X[sw_to_plot[i]], label=sw_to_plot[i], 
+                        color='black', linewidth=0.5)
+                ax[i+1].legend()
+                ax[i+1].xaxis.set_major_locator(locator)
+                ax[i+1].xaxis.set_major_formatter(formatter)
+                       
+            fig.tight_layout()
         return fig, ax
+    
+    def _predict_persistence(self, X, y):
+        y_pred = y.shift(periods=self.pred_step, freq='T')
+        if self.propagate:
+            pers_prop_time_processor = PropagationTimeProcessor(
+                Vx=X[self.vx_colname_], time_resolution=self.time_resolution,
+                D=self.D)
+            pers_prop_time_processor = pers_prop_time_processor.fit(y_pred)
+            y_pred = pd.Series(y_pred.values, 
+                index = pers_prop_time_processor.propagated_times_.values)
+            
+            # y_pred = pers_prop_time_processor.shift(y_pred)
+            
+        return y_pred
 
-    def score(self, X, y, squared=False):
-        y_pred = self.predict(X, y)
+    def score(self, X, y, negative=True, persistence=False, squared=False, 
+              round=False, **round_params):
+        
+        if persistence:
+            y_pred = self._predict_persistence(X, y)
+        else:
+            y_pred = self.predict(X, y)
 
+        score = self.score_func(y, y_pred, squared=squared)
+        if round:
+            score = np.around(score, **round_params)
+
+        if negative:
+            return -score
+        else:
+            return score
+    
+    def score_func(self, y, y_pred, **kwargs):
         y_ = y.reindex(y_pred.index)
         nan_mask = ~np.isnan(y_)
-
-        return mean_squared_error(
-            y_[nan_mask], y_pred[nan_mask], squared=squared)
+        
+        return mean_squared_error(y_[nan_mask], y_pred[nan_mask], **kwargs)
 
     def _more_tags(self):
         return {'allow_nan': True,
@@ -316,44 +409,80 @@ class GeoMagTSRegressor(BaseEstimator, RegressorMixin):
 
 # estimator_checks = check_estimator(GeoMagTSRegressor())
 
+
 class GeoMagARX(GeoMagTSRegressor):
     def __init__(self, auto_order=10,
                  exog_order=10,
                  pred_step=1,
                  transformer_X=None,
                  transformer_y=None,
-                 **lm_params):
-        base_estimator = LinearRegression()
-        super().__init__(base_estimator=base_estimator, auto_order=auto_order, exog_order=exog_order,
-                         pred_step=pred_step, transformer_X=transformer_X, transformer_y=transformer_y, **lm_params)
+                 lasso=False,
+                 **params):
+        if lasso:
+            base_estimator = Lasso()
+        else:
+            base_estimator = LinearRegression()
+        super().__init__(
+            base_estimator=base_estimator,
+            auto_order=auto_order,
+            exog_order=exog_order,
+            pred_step=pred_step,
+            transformer_X=transformer_X,
+            transformer_y=transformer_y,
+            **params)
 
-    def get_coef_df(self, feature_columns):
+    def get_coef_df(self, include_interactions=False):
         check_is_fitted(self)
-
+        
         ar_coef_names = np.array(
             ["ar"+str(i) for i in range(self.auto_order_steps_)]
         )
         exog_coef_names = np.concatenate(
             [[x+str(i) for i in range(self.exog_order_steps_)]
-             for x in feature_columns]
+             for x in self.features_]
         ).T
+        
         coef_names = np.concatenate([ar_coef_names, exog_coef_names])
 
         coef = pd.Series(
-            self.base_estimator_fitted_.coef_, index=coef_names)
+            self.base_estimator_fitted_.coef_[0:len(coef_names)], 
+            index=coef_names)
         coef_df = pd.DataFrame(
-            {col: coef[coef.index.str.contains(col+'[0-9]+$')].reset_index(drop=True)
-             for col in ['ar']+feature_columns
+            {col: coef[coef.index.str.contains('^'+col+'[0-9]+$')].reset_index(drop=True)
+             for col in self.features_.insert(0,'ar')
              }
         )
+        
+        if include_interactions:
+            
+            if isinstance(self.transformer_X, Pipeline):
+                transformers = [step[0] for step in self.transformer_X.steps]
+                if not np.isin(transformers, 'polynomialfeatures').any():
+                    raise ValueError("Interaction terms were not computed.")
+                powers = self.transformer_X['polynomialfeatures'].powers_
+            else:
+                if not isinstance(self.transformer_X, PolynomialFeatures):
+                    raise ValueError("Interaction terms were not computed.")
+                powers = self.transformer_X.powers_
+            
+            n_features = self.features_.shape[0]
+            colnames = self.features_.insert(0, 'ar')
+            interaction_masks = powers[n_features+1:].astype(bool)
+            interaction_colnames = ['_'.join(colnames[mask].tolist())
+                                 for mask in interaction_masks]
+            interaction_names = np.concatenate(
+                [[x+str(i) for i in range(self.exog_order_steps_)]
+                 for x in interaction_colnames]
+            )
+            interactions = pd.Series(
+                self.base_estimator_fitted_.coef_[len(coef_names):],
+                index=interaction_names
+            )
+            interactions_df = pd.DataFrame(
+                {col: interactions[interactions.index.str.contains('^'+col+'[0-9]+$')].reset_index(drop=True)
+                 for col in interaction_colnames
+                 }
+            )
+            coef_df = pd.concat([coef_df, interactions_df], axis=1)  
 
         return coef_df
-
-
-class PersistenceModel(BaseEstimator, RegressorMixin):
-    def __init__(self, pred_step=1,
-                 transformer_X=None,
-                 transformer_y=None):
-        self.pred_step = pred_step
-        self.transformer_X = transformer_X
-        self.transformer_y = transformer_y
